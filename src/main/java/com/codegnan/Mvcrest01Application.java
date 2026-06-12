f698a3677d1be431dcceb9c7fac78fa24bf47a1c
Hanisha01 <hanishavyda@gmail.com>
2026-06-12 11:33:09 +0530

    first commit

---
+++ src/main/java/com/codegnan/Mvcrest01Application.java
@@ -0,0 +1,13 @@
+package com.codegnan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvcrest01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvcrest01Application.class, args);
+	}
+
+}
